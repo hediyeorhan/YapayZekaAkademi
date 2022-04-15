a2a47d3bf77489702403204e8583427a069ac53c
Hediye Orhan <hediyeorhan2015@gmail.com>
2022-04-15 14:35:45 +0300

    Update Question1.py

--- Question1.py
+++ Question1.py
@@ -15,24 +15,35 @@ def compareTriplets(a, b):
 
     return [aliceScore, bobScore]
 
+def main():
+    a = [0,0,0]
+    b = [0,0,0]
+    i=0
+    j=0 
 
-a = [0,0,0]
-b = [0,0,0]
-i=0
-j=0
+    print(" -- Enter three values ​​for Alice --")
+    for i in range (0, 3):
+        a[i] = int(input("--> a{} : ".format(i)))
+        if(a[i]>100 or a[i]<0):
+            print("\n ERROR : Please enter values between 0 and 100 !!\n")
+            return
 
-print("Enter three values ​​for Alice : ")
-for i in range (0, 3):
-    a[i] = int(input("a{} : ".format(i)))
+    print(" -- Enter three values ​​for Bob --")
+    for j in range (0, 3):
+        b[j] = int(input("--> b{} : ".format(j)))
+        if(b[j]>100 or b[j]<0):
+            print("\n ERROR : Please enter values between 0 and 100 !!\n")
+            return
 
-print("Enter three values ​​for Bob : ")
-for j in range (0, 3):
-    b[j] = int(input("b{} : ".format(j)))
+
+    outcome = compareTriplets(a,b)
+
+    print("\n ----------------- \n\n")
+    print(" - Alice Score : {}\n - Bob Score : {}".format(outcome[0], outcome[1]))
+    print(outcome)
+    print("\n\n ----------------- \n")
 
 
-outcome = compareTriplets(a,b)
 
-print("\n ----------------- \n")
-print("Alice Score : {}\nBob Score : {}".format(outcome[0], outcome[1]))
-print(outcome)
-print("\n ----------------- \n")
+
+main()
